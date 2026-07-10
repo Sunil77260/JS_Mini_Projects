ee55d902b068a75d5e6956066b0b8825c8018bb9
Sunil Bishnoi <sunilbishnoi@Sunils-MacBook-Pro.local>
2026-07-10 15:49:38 +0530

    gitital clock mini project added

---
+++ 05-Gigital-Clock/index.js
@@ -0,0 +1,25 @@
+
+const day = document.querySelector('#day');
+const hours = document.querySelector('#hours');
+const mintues = document.querySelector('#mintues');
+const sec = document.querySelector('#sec');
+// const milisec = document.querySelector('#milisec');
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Throusday',
+    'Friday',
+    'Saturday'
+]
+
+setInterval(()=>{
+   const timer= new Date()
+   day.textContent = days[timer.getDay()];
+   hours.textContent = timer.getHours();
+   mintues.textContent =timer.getMinutes();
+   sec.textContent=timer.getSeconds();
+//    milisec.textContent=timer.getMilliseconds();
+},1)
