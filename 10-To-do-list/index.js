8d04bfce3e0227a9b5dcf7a9c54d9431bf3809df
Sunil Bishnoi <sunilbishnoi@Sunils-MacBook-Pro.local>
2026-07-14 23:26:23 +0530

    delete and done functionality added for to-do-list project

--- 10-To-do-list/index.js
+++ 10-To-do-list/index.js
@@ -10,7 +10,44 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
     const taskInput= task.value 
     const li =document.createElement('li');
-    li.innerText= taskInput;
+    const span = document.createElement('span')
+    const doneButton = document.createElement('button')
+    doneButton.textContent= "Done"
+    doneButton.style.color='white'
+    doneButton.style.height='40px';
+    doneButton.style.width="80px"
+    doneButton.style.backgroundColor='green'
+    doneButton.style.paddingLeft="5px"
+    doneButton.style.paddingRight="5px"
+    doneButton.style.paddingTop="2px"
+    doneButton.style.paddingBottom="2px"
+    doneButton.style.marginLeft="30px"
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent= "Delete"
+    deleteButton.style.color='white'
+    deleteButton.style.height='40px';
+    deleteButton.style.width="80px"
+    deleteButton.style.backgroundColor='red'
+    deleteButton.style.paddingLeft="5px"
+    deleteButton.style.paddingRight="5px"
+    deleteButton.style.paddingTop="2px"
+    deleteButton.style.paddingBottom="2px"
+    deleteButton.style.marginLeft="30px"
+
+    span.innerText= taskInput;
+
+    li.append(span,doneButton,deleteButton);
+
    list.appendChild(li)
+    
+   doneButton.addEventListener('click',()=>{
+    li.style.background='grey'
+   })
+
+   deleteButton.addEventListener('click',()=>{
+    li.remove();
+   })
+
    form.reset()
 })
